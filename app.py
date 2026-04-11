31494229f47c1150f0f29df1cce709261f57ec38
harveysmith1998 <harveysmith1998@gmail.com>
2026-04-11 20:45:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 BOT_TOKEN = "8755949206:AAG_6J4Vx7YfHv-yg_eA1t_AlIOQKX3hsag"
 CHAT_ID = "-1003787596424"
-data = request.get_json(force=True)
+
 
 @app.route("/", methods=["POST"])
 def webhook():
-    data = request.json
+    data = request.get_json(force=True)
     print("🔥 RECEIVED:", data)
 
     if data:
